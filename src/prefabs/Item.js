b073bac217792994888a0defa1d73d97be08402e
Wendy <wchen188@ucsc.edu>
2023-06-14 15:02:48 -0700

    added watch

--- src/prefabs/Item.js
+++ src/prefabs/Item.js
@@ -1,12 +1,17 @@
 class Item extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y, texture, points, name) {
-        super(scene, x, y, texture, points, name);
+    constructor(scene, x, y, texture, frame, points, name) {
+        super(scene, x, y, texture, frame);
         this.parentScene = scene;
         this.parentScene.add.existing(this);
         //this.parentScene.physics.add.existing(this);
         this.x = x;
         this.y = y;
-        this.setScale(0.2);
+        if (name == 'wallet') {
+            this.setScale(0.2);
+        } else if (name == 'watch') {
+            this.setScale(0.3);
+        }
+        
         this.points = points;
         this.name = name;
 
@@ -21,15 +26,17 @@ class Item extends Phaser.Physics.Arcade.Sprite {
         //this.parentScene.sound.play('collect');
         this.parentScene.incrementScore(this.points); 
         this.destroy();
-        console.log('left',itemsLeft);
-        console.log('num',numItems);
+    
+        
         if (itemsLeft < numItems) {
             this.parentScene.addItem();
-            console.log('add');
+            itemsLeft += 1;
+            //console.log('add');
         }
         if (itemsLeft < numItems) {
             setTimeout(() => {
                 this.parentScene.addItem();
+                itemsLeft += 1;
             }, 5000);
         }
     }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,15 +4,18 @@ class Play extends Phaser.Scene {
     }
     preload() {
         this.load.image('wallet', './assets/wallet.png');
+        this.load.image('watch', './assets/watch.png');
+        this.load.spritesheet('watch-anim', './assets/watch-anim.png', {frameWidth: 640, frameHeight: 480, startFrame: 0, endFrame: 8});
+
     }
     create(){
         //this.add.text(game.config.width/2, game.config.height/2 - 10, 'Next Pickpocket Scene').setOrigin(0.5);
         this.score = 0;
-        numItems = Phaser.Math.Between(25, 35);
+        numItems = Phaser.Math.Between(25, 30);
         //for (let i = 0; i < (numItems); i += 1){
             setTimeout(() => {
                 this.addItem();
-                this.itemsLeft += 1;
+                itemsLeft += 1;
             }, 1000);
             
         //}
@@ -35,6 +38,14 @@ class Play extends Phaser.Scene {
         this.timeLeft.setOrigin(0.5, 0.5);
         this.timeLeft.setFontSize(30);
 
+        this.anims.create({
+            key: 'watch-fade',
+            frames: this.anims.generateFrameNumbers('watch-anim', { start: 0, end: 8, first: 0}),
+            frameRate: 12
+        });
+
+        this.itemsLeft = this.add.text(borderUISize + borderPadding, game.config.height/2 - 40, itemsLeft, scoreConfig).setOrigin(0.5);
+
         this.clock = this.time.delayedCall(25000, () => {
             this.add.text(game.config.width/2, game.config.height/2 - 10, 'TIMES UP', scoreConfig).setOrigin(0.5);
         }, null, this);
@@ -42,16 +53,39 @@ class Play extends Phaser.Scene {
     update(){
             
         this.timeLeft.text = Math.trunc(this.clock.getOverallRemainingSeconds());
+        //itemsLeft.text = itemsLeft;
         
     }
     addItem(){
-        let item = new Item(this, Phaser.Math.Between(0, this.game.config.width), Phaser.Math.Between(0, this.game.config.height), 'wallet', 5, 'wallet');
-        this.physics.add.existing(item);
+        let item1 = new Item(this, Phaser.Math.Between(0, this.game.config.width), Phaser.Math.Between(0, this.game.config.height), 'wallet', 0, 2, 'wallet');
+        let item2 = new Item(this, Phaser.Math.Between(0, this.game.config.width), Phaser.Math.Between(0, this.game.config.height), 'watch', 0, 1, 'watch');
+        let item = Phaser.Math.Between(1,2);
+        if (itemsLeft < numItems) {
+            if (item == 1){
+                this.physics.add.existing(item1);
+                this.item1 = item1;
+                //itemsLeft += 1;
+            } else {
+                this.physics.add.existing(item2);
+                this.item2 = item2;   
+                //itemsLeft += 1;
+            }
+        }
+        this.itemsLeft.text = itemsLeft;
+        console.log('left',itemsLeft);
+        console.log('num',numItems);
     }
     incrementScore(points) {
         this.score += points;
         this.scoreLeft.text = this.score;
-        itemsLeft += 1;
-        //this.scoreText.setText('Score: ' + this.score); // Update the score text
+    }
+    takeWatch() {
+        this.item2.setVisible(false);
+        let boom = this.add.sprite(this.item2.x, this.item2.y, 'watch-anim').setOrigin(0, 0);
+        boom.anims.play('watch-fade');             // play fade animation
+        boom.on('animationcomplete', () => {    // callback after anim completes
+            //this.item2.setVisible(true);                   // make visible again
+            this.item2.destroy();                       // remove explosion sprite
+        });
     }
 }
