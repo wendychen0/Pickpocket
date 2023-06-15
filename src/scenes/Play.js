c818b747b5b93a70ed4d0f2d99e02a945018f929
Wendy <wchen188@ucsc.edu>
2023-06-14 20:42:34 -0700

    final game

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,10 +9,11 @@ class Play extends Phaser.Scene {
 
     }
     create(){
-        //this.add.text(game.config.width/2, game.config.height/2 - 10, 'Next Pickpocket Scene').setOrigin(0.5);
         this.score = 0;
         this.gameOver = false;
         this.passed = false;
+        itemsLeft = 0;
+        numItems = 0;
         numItems = Phaser.Math.Between(27, 35);
         setTimeout(() => {
             this.addItem();
@@ -47,15 +48,16 @@ class Play extends Phaser.Scene {
             frameRate: 12
         });
 
-        this.itemsLeft = this.add.text(borderUISize + borderPadding, game.config.height/2 - 40, itemsLeft, scoreConfig).setOrigin(0.5);
+        //this.itemsLeft = this.add.text(borderUISize + borderPadding, game.config.height/2 - 40, itemsLeft, scoreConfig).setOrigin(0.5);
 
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
-        this.clock = this.time.delayedCall(20000, () => {
-            //this.add.text(game.config.width/2, game.config.height/2 - 10, 'TIMES UP', scoreConfig).setOrigin(0.5);
+        this.clock = this.time.delayedCall(21000, () => {
+
         }, null, this);
+        this.clock.paused = false;
     }
     update(){
             
@@ -73,22 +75,23 @@ class Play extends Phaser.Scene {
             },
         }
         this.timeLeft.text = Math.trunc(this.clock.getOverallRemainingSeconds());
-        this.itemsLeft.text = itemsLeft;
+        //this.itemsLeft.text = itemsLeft;
 
         // timer for info text
-        if (Math.trunc(this.clock.elapsed/1000) == 7){//} && this.reached) {
+        if (Math.trunc(this.clock.elapsed/1000) == 7){
             this.infoText.setVisible(false);
             this.infoText2.setVisible(false);
         }
 
-        if (Math.trunc(this.clock.elapsed/1000) == 20 && this.score < itemsLeft - 1) {
+        if (Math.trunc(this.clock.elapsed/1000) == 21 && this.score < itemsLeft - 1) {
             this.add.text(game.config.width/2, game.config.height/2 - 10, 'TIMES UP', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 54, 'Press (R) to Try Again or ← for Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
-        } else if (Math.trunc(this.clock.elapsed/1000) == 20 && this.score == itemsLeft - 1){
-            this.add.text(game.config.width/2, game.config.height/2 - 10, 'Congrats', scoreConfig).setOrigin(0.5);
+        } else if (Math.trunc(this.clock.elapsed/1000) == 21 || this.score == itemsLeft - 1){
+            this.add.text(game.config.width/2, game.config.height/2 - 10, 'Congrats you have mastered the skill', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 54, 'Press ← to return to Menu', scoreConfig).setOrigin(0.5);
             this.passed = true;
+            this.clock.paused = true;
         }
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.start("trainingScene");
@@ -97,13 +100,14 @@ class Play extends Phaser.Scene {
         if ((this.gameOver || this.passed) && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
             this.scene.start("menuScene");
             this.gameOver = false;
+            this.passed = false;
         }
         
     }
     addItem(){
         if (itemsLeft < numItems && (!this.gameOver || !this.passed)) {
-            let item1 = new Item(this, Phaser.Math.Between(5, this.game.config.width), Phaser.Math.Between(4, this.game.config.height - 3), 'wallet', 0, 1, 'wallet');
-            let item2 = new Item(this, Phaser.Math.Between(5, this.game.config.width), Phaser.Math.Between(4, this.game.config.height - 3), 'watch', 0, 1, 'watch');
+            let item1 = new Item(this, Phaser.Math.Between(7, this.game.config.width - 5), Phaser.Math.Between(6, this.game.config.height - 4), 'wallet', 0, 1, 'wallet');
+            let item2 = new Item(this, Phaser.Math.Between(7, this.game.config.width - 6), Phaser.Math.Between(6, this.game.config.height - 6), 'watch', 0, 1, 'watch');
             let item = Phaser.Math.Between(1,2);
             if (item == 1){
                 this.physics.add.existing(item1);
@@ -115,7 +119,7 @@ class Play extends Phaser.Scene {
                 itemsLeft += 2;
             }
         }
-        this.itemsLeft.text = itemsLeft;
+        //this.itemsLeft.text = itemsLeft;
     }
     incrementScore(points) {
         this.score += points;
